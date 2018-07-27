eaa4fed2688809e3cae61f1ed9cd6d71cc0048c2
Ryan Pencak <rvp002@bucknell.edu>
2018-07-27 16:12:45 -0500

    Score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text outputScore;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setScore();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -21,5 +28,25 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setScore();
+        }
+        Destroy(other.gameObject);
+    }
+
+    void setScore()
+    {
+        outputScore.text = "Count: " + count.ToString();
+        if (count == 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
